edaba8148782624c781cbc41ed48c963ae1c440f
francescogentile25 <francescogentile250893@gmail.com>
2023-02-03 16:13:15 +0100

    ERRORE 2

--- js/game.js
+++ js/game.js
@@ -8,14 +8,29 @@ dataBaseMail.push(mailInput)
 // console.log (dataBaseMail)
 // per ogni email (for) devo controllare se esiste (if) altrimenti (else) aggiungo all array l email
 
-let emailCeck = false;
+// let emailCeck = false;
+// for (let i = 0; i < dataBaseMail.length; i++) {
+//     if (dataBaseMail[i] === mailInput) {
+//         console.log('Email presente');
+//     }else{
+//         console.log('Email non trovata');
+//         dataBaseMail.push(mailInput);
+//         console.log('Email agigunta alla lista ');
+//     }
+// }    
+
+
+let emailFound =false
 for (let i = 0; i < dataBaseMail.length; i++) {
     if (dataBaseMail[i] === mailInput) {
+        let emailFound=true
         console.log('Email presente');
-    }else{
-        console.log('Email non trovata');
-        dataBaseMail.push(mailInput);
-        console.log('Email agigunta alla lista ');
     }
-}    
+}
 
+if (emailFound =true){
+    console.log('Email non trovata');
+    dataBaseMail.push(mailInput);
+    console.log('Email agigunta alla lista')
+    console.log(dataBaseMail)
+}
