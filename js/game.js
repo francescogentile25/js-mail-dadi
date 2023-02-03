f327c0e4f43532ad3399f62d83ef3eb04677975a
francescogentile25 <francescogentile250893@gmail.com>
2023-02-03 18:27:21 +0100

    FEAT ex finito

--- js/game.js
+++ js/game.js
@@ -4,33 +4,31 @@ const mailInput= prompt('Inserisci la tua mail')
 const dataBaseMail = ['me@gmail.com' , 'you@gmail.com', 'he@gmail.com']
 // console.log (dataBaseMail)
 
-dataBaseMail.push(mailInput)
-// console.log (dataBaseMail)
-// per ogni email (for) devo controllare se esiste (if) altrimenti (else) aggiungo all array l email
+let emailFound = false;
 
-// let emailCeck = false;
-// for (let i = 0; i < dataBaseMail.length; i++) {
-//     if (dataBaseMail[i] === mailInput) {
-//         console.log('Email presente');
-//     }else{
-//         console.log('Email non trovata');
-//         dataBaseMail.push(mailInput);
-//         console.log('Email agigunta alla lista ');
-//     }
-// }    
-
-
-let emailFound =false
 for (let i = 0; i < dataBaseMail.length; i++) {
     if (dataBaseMail[i] === mailInput) {
-        let emailFound=true
-        console.log('Email presente');
+        emailFound=true;
     }
 }
 
-if (emailFound =true){
+if (emailFound === false){
     console.log('Email non trovata');
     dataBaseMail.push(mailInput);
     console.log('Email agigunta alla lista')
-    console.log(dataBaseMail)
-}
+    console.log(dataBaseMail)}
+    else{
+        console.log('Email presente')
+    }
+
+
+    let  playerNumber= Math.floor(Math.random()* 6) +1
+    let  pcNumber= Math.floor(Math.random()* 6) +1
+    console.log(playerNumber, pcNumber)
+    if (playerNumber> pcNumber){
+        console.log('Hai vinto')
+    }else if (playerNumber < pcNumber){
+        console.log('Hai perso')
+    } else{
+        console.log('È un pareggio')
+    }
